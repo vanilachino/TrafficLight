7f6284d193bb647ab550dfe0f4510fada44d635f
John M <vanja.melnik7@gmail.com>
2021-03-24 21:41:41 +0200

    Second task traffic light implementation

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redCollorView: UIView!
+    @IBOutlet weak var yellowColorView: UIView!
+    @IBOutlet weak var greenColorView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var lightsArray: [UIView] = []
+    var currentLight = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lightsArray = [redCollorView, yellowColorView, greenColorView]
+        startButton.setTitle("START", for: .normal)
+        startButton.layer.cornerRadius = 10
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let radius = redCollorView.frame.height / 2
+        redCollorView.layer.cornerRadius = radius
+        yellowColorView.layer.cornerRadius = radius
+        greenColorView.layer.cornerRadius = radius
+    }
+    
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        let nextLight = currentLight + 1
+        
+        lightsArray[currentLight].alpha = 0.3
+        currentLight = nextLight == lightsArray.count ? 0 : nextLight
+        
+        lightsArray[currentLight].alpha = 1.0
+    }
+    
 }
 
